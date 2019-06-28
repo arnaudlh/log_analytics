40421ec21f2d1a5560e214f2c4d531cf42d19e61
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-06-28 16:43:22 +0800

    Diags structure update and naming standardization

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "log_anal_workspace_id" {
+output "log_analytics_workspace_id" {
   depends_on = [azurerm_log_analytics_solution.container_insights]
   value      = azurerm_log_analytics_workspace.log.id
 }
 
-output "log_anal_workspace_name" {
+output "log_analytics_workspace_name" {
   depends_on = [azurerm_log_analytics_solution.container_insights]
   value      = azurerm_log_analytics_workspace.log.name
 }
